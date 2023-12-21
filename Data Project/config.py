ba25d60560b713cbfb4b42aa288e298352d99e54
Nurhak Said Çakır <nurhaksaidcakir@gmail.com>
2023-12-21 22:42:29 +0300

    Double Sided Data Transfer
    Data Transfer Between Excel and Database with Python

---
+++ Data Project/config.py
@@ -0,0 +1,17 @@
+
+database_name = "dbodoo16"
+user = "postgres"
+password = "Nurhak123"
+host = "localhost"
+port = 5432
+
+file_path1 = "to_account_move.xlsx"
+table_name1 = "account_move"
+file_path2 = "to_account_move_line.xlsx"
+table_name2 = "account_move_line"
+sheet_name="Sheet1"
+start_row=0
+finish_row=4
+start_column=0
+finish_column=70
+excel_path ="to_account_move_line.xlsx"
